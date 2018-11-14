df1491116ea0b8ec0b625b7283104b0ec64c3c3f
439002 <439002@PC376563.cts.com>
2018-11-14 18:40:40 +0530

    FirstCommit

---
+++ OwnFramework/src/main/java/excelModules/ExcelAccess.java
@@ -0,0 +1,20 @@
+package excelModules;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelAccess {
+
+	XSSFWorkbook wb;
+	XSSFSheet ws;
+	XSSFRow row;
+	XSSFCell cell;
+	
+	public ExcelAccess(String path)
+	{
+		
+	}
+	
+}
